c57b9affce9039af820d70177c449c30f4e08370
RamaDev17 <ramdhaney.dev17@gmail.com>
2024-11-25 06:35:10 +0700

    feat: change redirect to useRouter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
+import { checkAuth } from "@/helpers/checkAuth";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+
+const validateUser = async (router: AppRouterInstance | string[]) => {
+  const isAuthenticated = await checkAuth();
+  
+  if (isAuthenticated) {
+    
+    router.push("/admin/dashboard");
+  }else{
+    console.log('disinikah?');
+    router.push("/login")
+  }
+};
 
 export default function Home() {
-  return redirect("/login")
+  const router = useRouter();
+  useEffect(() => {
+    validateUser(router);
+  }, [router]);
+
+  return(
+    <div className="flex items-center justify-center h-screen">
+      <LoadingSpinner />
+    </div>
+  )
 }
