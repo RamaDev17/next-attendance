b0a992797c3fe37c054e640fdd7134e4ba6f83fa
RamaDev17 <ramdhaney.dev17@gmail.com>
2024-11-30 08:57:58 +0700

    feat: BREAD offices

--- src/app/admin/office/page.tsx
+++ src/app/admin/office/page.tsx
@@ -1,299 +1,256 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Plus } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu";
-// import { Badge } from "@/components/ui/badge"
-// import { Checkbox } from "@/components/ui/checkbox";
-// import { useApi } from "@/hooks/use-api";
-import { useToast } from "@/hooks/use-toast";
+import React, { useState } from "react";
+import { Button, Input, Modal, Table, Form, message, Popconfirm } from "antd";
+import type { TableColumnsType, TablePaginationConfig } from "antd";
 import { useCrud } from "@/hooks/use-crud";
 import { OfficeType } from "@/types";
-// import { Badge } from "@/components/ui/badge";
+import {
+  SearchOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
 
-export default function Office() {
-  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(10);
+const columnsTemplate: TableColumnsType<OfficeType> = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    width: 250,
+  },
+  {
+    title: "Latitude",
+    dataIndex: "latitude",
+    key: "latitude",
+    width: 150,
+  },
+  {
+    title: "Longitude",
+    dataIndex: "longitude",
+    key: "longitude",
+    width: 150,
+  },
+  {
+    title: "Radius",
+    dataIndex: "radius",
+    key: "radius",
+    width: 150,
+  },
+];
 
-  const { toast } = useToast();
-
-  const { useFetchAll, useCreate } = useCrud("offices");
-  const createMutation = useCreate();
-
-  const { data, isLoading, error } = useFetchAll({ page, limit }) as {
-    data: { data: OfficeType[]; message: string; pagination: any };
-    isLoading: boolean;
-    error: any;
-  };  
-
-  // Total halaman dari response API
-  const totalPages = data?.pagination.totalPages || 1;
-
-  // Event handler untuk mengganti limit
-  const handleLimitChange = (newLimit: number) => {
-    console.log(newLimit);
-    
-    setLimit(newLimit);
-    setPage(1); // Reset ke halaman pertama saat limit berubah
+interface Data {
+  data: OfficeType[];
+  pagination: {
+    totalItems: number;
   };
+}
 
-  // Event handler untuk mengganti halaman
-  const handlePageChange = (newPage: number) => {
-    setPage(newPage);
-  };
-  //   console.log(data.data.leng);
-
-  const handleCreateTask = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const newTask: OfficeType = {
-      name: formData.get("name") as string,
-      latitude: Number(formData.get("latitude")),
-      longitude: Number(formData.get("longitude")),
-      radius: Number(formData.get("radius")),
+interface ErrorResponse {
+  response: {
+    data: {
+      message: string;
     };
+  };
+}
 
-    createMutation.mutate(newTask, {
-      onSuccess: () => {
-        toast({
-          title: "Success",
-          description: "Office created successfully",
-          variant: "default",
-        });
-      },
-      onError: () => {
-        toast({
-          title: "Error",
-          description: "Failed to create office",
-          variant: "destructive",
-        });
-      },
+const App: React.FC = () => {
+  const { useFetchAll, useCreate, useUpdate, useDelete } = useCrud("offices");
+
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+  });
+  const [keyword, setKeyword] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [form] = Form.useForm();
+  const [editingRecord, setEditingRecord] = useState<OfficeType | null>(null);
+
+  const { data, isLoading } = useFetchAll({
+    page: pagination.current,
+    limit: pagination.pageSize,
+    keyword,
+  }) as { data: Data; isLoading: boolean };
+
+  const createOffice = useCreate();
+  const updateOffice = useUpdate();
+  const deleteOffice = useDelete();
+
+  const handleTableChange = (pagination: TablePaginationConfig) => {
+    setPagination({
+      current: pagination.current as number,
+      pageSize: pagination.pageSize as number,
     });
-
-    setIsCreateModalOpen(false);
   };
 
-  //   const handleEditTask = (event: React.FormEvent<HTMLFormElement>) => {
-  //     event.preventDefault()
-  //     if (!selectedTask) return
+  const handleAdd = () => {
+    setIsEdit(false);
+    form.resetFields();
+    setIsModalOpen(true);
+  };
 
-  //     const formData = new FormData(event.currentTarget)
-  //     const updatedTask: Task = {
-  //       ...selectedTask,
-  //       type: formData.get("type") as Task["type"],
-  //       title: formData.get("title") as string,
-  //       status: formData.get("status") as Task["status"],
-  //       priority: formData.get("priority") as Task["priority"],
-  //     }
+  const handleEdit = (record: OfficeType) => {
+    setIsEdit(true);
+    setEditingRecord({
+      id: record.id,
+      name: record.name,
+      latitude: parseFloat(String(record.latitude)),
+      longitude: parseFloat(String(record.longitude)),
+      radius: Number(record.radius),
+    });
+    form.setFieldsValue(record);
+    setIsModalOpen(true);
+  };
+  
+  const handleSubmit = async (values: OfficeType) => {
+    try {
+      if (isEdit && editingRecord) {
+        await updateOffice.mutateAsync({
+          id: editingRecord.id ?? "",
+          payload: values,
+        });
+        message.success("Office updated successfully");
+      } else {
+        await createOffice.mutateAsync(values);
+        message.success("Office added successfully");
+      }
+      setIsModalOpen(false);
+      form.resetFields();
+    } catch (error) {
+      const errorResponse = error as ErrorResponse;
+      message.error(
+        "Failed to save office : " + errorResponse.response.data.message || ""
+      );
+    }
+  };
 
-  //     setTasks(tasks.map(task => task.id === selectedTask.id ? updatedTask : task))
-  //     setIsEditModalOpen(false)
-  //     setSelectedTask(null)
-  //   }
+  const columns: TableColumnsType<OfficeType> = [
+    ...columnsTemplate,
+    {
+      title: "Actions",
+      key: "actions",
+      width: 100,
+      render: (_, record) => (
+        <div style={{ display: "flex", gap: "15px" }}>
+          <Button
+            type="link"
+            icon={<EditOutlined />}
+            onClick={() => handleEdit(record)}
+          />
+          <Popconfirm
+            title="Are you sure?"
+            okText="Yes"
+            cancelText="No"
+            onConfirm={async () => {
+              try {
+                await deleteOffice.mutateAsync(record.id ?? "");
+                message.success("Office deleted successfully");
+              } catch (error) {
+                const errorResponse = error as ErrorResponse;
+                message.error(
+                  "Failed to save office : " +
+                    errorResponse.response.data.message || ""
+                );
+              }
+            }}
+          >
+            <Button type="link" danger icon={<DeleteOutlined />} />
+          </Popconfirm>
+        </div>
+      ),
+    },
+  ];
 
   return (
-    <div className="container mx-auto">
-      <div className="flex justify-between items-center mb-8">
-        <div>
-          <h1 className="text-2xl font-bold">Welcome back!</h1>
-          <p className="text-muted-foreground">
-            Heres a list of your tasks for this month!
-          </p>
-        </div>
-        <Dialog open={isCreateModalOpen} onOpenChange={setIsCreateModalOpen}>
-          <DialogTrigger asChild>
-            <Button>
-              <Plus className="mr-2 h-4 w-4" />
-              Add Task
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Create New Task</DialogTitle>
-            </DialogHeader>
-            <form
-              onSubmit={handleCreateTask}
-              method="POST"
-              encType="multipart/form-data"
-              className="space-y-4"
-            >
-              <div>
-                <label className="text-sm font-medium">Name</label>
-                <Input name="name" required />
-              </div>
-              <div>
-                <label className="text-sm font-medium">Latitude</label>
-                <Input name="latitude" type="number" required />
-              </div>
-              <div>
-                <label className="text-sm font-medium">Longitude</label>
-                <Input name="longitude" type="number" required />
-              </div>
-              <div>
-                <label className="text-sm font-medium">Radius</label>
-                <Input name="radius" type="number" required />
-              </div>
-
-              <Button type="submit" className="w-full">
-                Create Task
-              </Button>
-            </form>
-          </DialogContent>
-        </Dialog>
+    <div>
+      <div className="mb-4 mt-2 flex items-center justify-between">
+        <Input
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder="Search by name"
+          size="middle"
+          prefix={<SearchOutlined />}
+          allowClear
+          style={{ width: 300 }}
+        />
+        <Button type="primary" size="middle" onClick={handleAdd}>
+          Add Office
+        </Button>
       </div>
-
-      <div className="space-y-4">
-        <div className="flex items-center gap-4">
-          <Input placeholder="Search offices" className="max-w-sm" />
-        </div>
-
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>no</TableHead>
-              <TableHead>name</TableHead>
-              <TableHead>latitide</TableHead>
-              <TableHead>longitude</TableHead>
-              <TableHead>radius</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data &&
-              data.data.map((data, index) => (
-                <TableRow key={data.id}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>{data.name}</TableCell>
-                  <TableCell>{data.latitude}</TableCell>
-                  <TableCell>{data.longitude}</TableCell>
-                  <TableCell>{data.longitude}</TableCell>
-                  <TableCell>{data.radius}</TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-
-        {/* Pagination */}
-        <Pagination className="gap-4 mt-4 flex justify-end">
-          <PaginationContent>
-            {/* Tombol Previous */}
-            <PaginationItem>
-              <PaginationPrevious
-                onClick={() => handlePageChange(Math.max(1, page - 1))}
-              />
-            </PaginationItem>
-
-            {/* Tombol Page Number */}
-            {Array.from({ length: totalPages }, (_, i) => (
-              <PaginationItem key={i + 1}>
-                <PaginationLink
-                  isActive={page === i + 1}
-                  onClick={() => handlePageChange(i + 1)}
-                >
-                  {i + 1}
-                </PaginationLink>
-              </PaginationItem>
-            ))}
-
-            {/* Tombol Next */}
-            <PaginationItem>
-              <PaginationNext
-                onClick={() => handlePageChange(Math.min(totalPages, page + 1))}
-              />
-            </PaginationItem>
-          </PaginationContent>
-
-          {/* Dropdown untuk jumlah item per halaman */}
-          <Select defaultValue="10" onValueChange={(value) => handleLimitChange(Number(value))}>
-            <SelectTrigger className="w-[70px]">
-              <SelectValue/>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                {[10, 20, 50, 100].map((option) => (
-                  <SelectItem
-                    key={option}
-                    value={option.toString()}
-                  >
-                    {option}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </Pagination>
-      </div>
-
-      <Dialog open={isEditModalOpen} onOpenChange={setIsEditModalOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Edit Task</DialogTitle>
-          </DialogHeader>
-          {/* {selectedTask && (
-            <form onSubmit={handleEditTask} className="space-y-4">
-              <div>
-                <label className="text-sm font-medium">Name</label>
-                <Input name="name" defaultValue={selectedTask.name} required />
-              </div>
-              <div>
-                <label className="text-sm font-medium">Latitude</label>
-                <Input name="latitude" type="number" defaultValue={selectedTask.latitude} required />
-              </div>
-              <div>
-                <label className="text-sm font-medium">Longitude</label>
-                <Input name="longitude" type="number" defaultValue={selectedTask.longitude} required />
-              </div>
-              <div>
-                <label className="text-sm font-medium">Radius</label>
-                <Input name="radius" type="number" defaultValue={selectedTask.radius} required />
-              </div>
-              
-              <Button type="submit" className="w-full">Update Task</Button>
-            </form>
-          )} */}
-        </DialogContent>
-      </Dialog>
+      <Table
+        columns={columns}
+        dataSource={data?.data}
+        loading={isLoading}
+        rowKey="id"
+        pagination={{
+          current: pagination.current,
+          pageSize: pagination.pageSize,
+          total: data?.pagination?.totalItems,
+          showSizeChanger: true,
+          pageSizeOptions: ["10", "20", "30", "40", "50"],
+        }}
+        onChange={handleTableChange}
+        bordered
+      />
+      <Modal
+        title={isEdit ? "Edit Office" : "Add Office"}
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        onOk={() => form.submit()}
+      >
+        <Form form={form} onFinish={handleSubmit} layout="vertical">
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[
+              {
+                required: isEdit ? false : true,
+                message: "Please input the name!",
+              },
+            ]}
+          >
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item
+            label="Latitude"
+            name="latitude"
+            rules={[
+              {
+                required: isEdit ? false : true,
+                message: "Please input the latitude!",
+              },
+            ]}
+          >
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item
+            label="Longitude"
+            name="longitude"
+            rules={[
+              {
+                required: isEdit ? false : true,
+                message: "Please input the longitude!",
+              },
+            ]}
+          >
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item
+            label="Radius"
+            name="radius"
+            rules={[
+              {
+                required: isEdit ? false : true,
+                message: "Please input the radius!",
+              },
+            ]}
+          >
+            <Input type="number" />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
-}
+};
+
+export default App;
